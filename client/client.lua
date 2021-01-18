87661c3d351160e296810e8c0879599899b7e1e1
fizzfau <59974801+fizzfau@users.noreply.github.com>
2021-01-18 20:36:19 +0300

    Add files via upload

---
+++ client/client.lua
@@ -0,0 +1,34 @@
+local table = {}
+
+function DrawNotify(id, text, color)
+	if id then
+		if table[id] == nil then
+			table[id] = text
+			print("+")
+			SendNUIMessage({display = true, text = text, color = color or "#26242f"})
+		end
+	end
+	return id
+end
+
+function Clear(id)
+	if table[id] ~= nil then
+		table[id] = nil
+		print("-")
+		SendNUIMessage({display = false})
+	end
+end
+
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
+		local ped = PlayerPedId()
+		local coords = GetEntityCoords(ped)
+		local distance = #(coords - vector3(465.5316, -234.332, 54.686))
+		if distance <= 7.5 then
+			DrawNotify("default", "[fizzfau-ui] message", nil)
+		else
+			Clear("default")
+		end
+	end
+end)
